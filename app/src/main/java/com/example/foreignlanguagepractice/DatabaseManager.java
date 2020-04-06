c4c73e69967e09e79df8b00616e6d640c15fd8a2
Gigara <info@gigara.info>
2020-04-07 00:10:30 +0530

    Add language subscription function

--- app/src/main/java/com/example/foreignlanguagepractice/DatabaseManager.java
+++ app/src/main/java/com/example/foreignlanguagepractice/DatabaseManager.java
@@ -3,41 +3,113 @@ package com.example.foreignlanguagepractice;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DatabaseManager extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "Translator.db";
-    private static final String TABLE_NAME = "PhraseList_table";
-    private static final String COL_1 = "Phrase";
+    private static final String PHRASE_LIST_TABLE = "PhraseList_table";
+    private static final String PHRASE = "Phrase";
+
+    private static final String LANGUAGES_TABLE = "languages";
+
+    private Context context1;
+    private static String DB_NAME = "db";
+    private static String DB_PATH;
+    private SQLiteDatabase languagesDB;
+
     SQLiteDatabase db = this.getWritableDatabase();
 
     DatabaseManager(Context context) {
         super(context, DATABASE_NAME, null, 1);
+        context1 = context;
+        DB_PATH = context1.getExternalFilesDir(null).getPath() + "/";
 
+        boolean dbexist = checkdatabase();
+        if(dbexist) {
+            System.out.println(" Database exists.");
+        } else {
+            this.getReadableDatabase();
+            try {
+                copydatabase();
+
+            } catch(IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+        opendatabase();
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (Phrase TEXT PRIMARY KEY)");
+        db.execSQL("CREATE TABLE " + PHRASE_LIST_TABLE + " (Phrase TEXT PRIMARY KEY)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+ PHRASE_LIST_TABLE);
         onCreate(db);
+    }
+
+    private boolean checkdatabase() {
+
+        boolean checkdb = false;
+        try {
+            String myPath = DB_PATH + DB_NAME;
+            File dbfile = new File(myPath);
+            checkdb = dbfile.exists();
+        } catch(SQLiteException e) {
+            System.out.println("Database doesn't exist");
+        }
+        return checkdb;
+    }
+
+    private void copydatabase() throws IOException {
+        //Open local db as the input stream
+        InputStream myinput = context1.getAssets().open(DB_NAME);
+
+        // Path to the just created empty db
+        String outfilename = DB_PATH + DB_NAME;
+
+        //Open the empty db as the output stream
+        OutputStream myoutput = new FileOutputStream(outfilename);
+
+        // transfer byte to inputfile to outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myinput.read(buffer))>0) {
+            myoutput.write(buffer,0,length);
+        }
+
+        //Close the streams
+        myoutput.flush();
+        myoutput.close();
+        myinput.close();
+    }
+
+    public void opendatabase() throws SQLException {
+        String mypath = DB_PATH + DB_NAME;
+        languagesDB = SQLiteDatabase.openDatabase(mypath, null, SQLiteDatabase.OPEN_READWRITE);
     }
 
     boolean insertData(String phrase){
         long result = 0;
-        String Query = "Select * from " + TABLE_NAME + " WHERE Phrase ='" + phrase + "'";
+        String Query = "Select * from " + PHRASE_LIST_TABLE + " WHERE Phrase ='" + phrase + "'";
         Cursor cursor = db.rawQuery(Query, null);
         if(cursor.getCount() <= 0){
             cursor.close();
             ContentValues contentValues = new ContentValues();
-            contentValues.put(COL_1,phrase);
+            contentValues.put(PHRASE,phrase);
 
-            db.insert(TABLE_NAME,null,contentValues);
+            db.insert(PHRASE_LIST_TABLE,null,contentValues);
             result = 1;
         }
         cursor.close();
@@ -47,22 +119,35 @@ public class DatabaseManager extends SQLiteOpenHelper {
             return true;
     }
 
+    boolean updateSubscription(String id, int subscribed){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("subscribed", subscribed);
+
+        languagesDB.update(LANGUAGES_TABLE, contentValues, "rowid = ?",new String[]{id});
+        return true;
+    }
+
     Cursor getAllData(){
-        Cursor res = db.rawQuery("SELECT rowid, * FROM "+TABLE_NAME+" ORDER BY Phrase COLLATE NOCASE ",null);
+        Cursor res = db.rawQuery("SELECT rowid, * FROM "+ PHRASE_LIST_TABLE +" ORDER BY Phrase COLLATE NOCASE ",null);
+        return res;
+    }
+
+    Cursor getAllLangs() {
+        Cursor res = languagesDB.rawQuery("SELECT rowid, * FROM "+ LANGUAGES_TABLE,null);
         return res;
     }
 
     boolean updateData(String phrase, String id){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_1,phrase);
+        contentValues.put(PHRASE,phrase);
 
-        db.update(TABLE_NAME, contentValues, "rowid = ?",new String[]{id});
+        db.update(PHRASE_LIST_TABLE, contentValues, "rowid = ?",new String[]{id});
         return true;
     }
 
     public Cursor search(String phrase){
-        Cursor res = db.rawQuery("SELECT * FROM "+TABLE_NAME+" WHERE Phrase LIKE "+phrase,null);
+        Cursor res = db.rawQuery("SELECT * FROM "+ PHRASE_LIST_TABLE +" WHERE Phrase LIKE "+phrase,null);
         return res;
     }
 }
--- app/src/main/java/com/example/foreignlanguagepractice/LanguageSubscription.java
+++ app/src/main/java/com/example/foreignlanguagepractice/LanguageSubscription.java
@@ -1,14 +1,90 @@
 package com.example.foreignlanguagepractice;
 
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.foreignlanguagepractice.MainActivity.phraseDatabase;
+
 public class LanguageSubscription extends AppCompatActivity {
+
+    ListView languageList;
+    List<String> languages = new ArrayList<>();
+    List<Integer> languageSubscriptions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_subscription);
+        languageList = findViewById(R.id.lang_list);
+        languageList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+        loadData();
+    }
+
+    public void loadData() {
+        Cursor res = phraseDatabase.getAllLangs();
+
+        if (res.getCount() == 0) {
+            Toast.makeText(LanguageSubscription.this, "Nothing to show", Toast.LENGTH_LONG).show();
+        } else {
+            while (res.moveToNext()) {
+                String phrase = res.getString(1);
+                languages.add(phrase);
+
+                int itemId = res.getInt(3);
+                languageSubscriptions.add(itemId);
+            }
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.checkable_list_layout, R.id.txt_title, languages) {
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+
+                    View view = super.getView(position, convertView, parent);
+                    TextView text = (TextView) view.findViewById(R.id.txt_title);
+
+                    text.setTextColor(Color.WHITE);
+                    return view;
+                }
+            };
+            languageList.setAdapter(arrayAdapter);
+
+            for (int i = 0; i < languageSubscriptions.size(); i++) {
+                if (languageSubscriptions.get(i) == 1) {
+                    languageList.setItemChecked(i, true);
+                }
+            }
+
+            languageList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    // selected item
+//                    selectedPosition = position;
+                }
+            });
+        }
+    }
+
+    public void update(View view) {
+        SparseBooleanArray arr = languageList.getCheckedItemPositions();
+        for (int i =0; i<languageList.getCount(); i++) {
+            int subscribed = arr.get(i) ? 1 : 0;
+            languageSubscriptions.set(i, subscribed);
+            phraseDatabase.updateSubscription(String.valueOf(i + 1), subscribed);
+        }
+        Toast.makeText(this, "Updated Successfully", Toast.LENGTH_LONG).show();
     }
 }
